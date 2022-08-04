2b22cd085c74018d384edade9667e9cc6f94c3cc
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-08-04 23:35:44 +0200

    Added install.sh file

---
+++ installer.sh
@@ -0,0 +1,6 @@
+
+# This is a script to install development tools for mac os x.
+# First step is to download the Mac OS X Developer Tools.
+
+xcode-select --install  # Install Xcode command line tools. This is a must for the following steps.
+# should i install this through brew as well ?
